eab9b760521a7148cd221dcf1d4ae0b18497f350
Anton Panchenko <apanchenko@gmail.com>
2019-04-24 19:04:12 +0300

    forget jupyter, making py script

--- titanic/src/titanic.py
+++ titanic/src/titanic.py
@@ -1,94 +1,159 @@
+# 04/2019 Anton Panchenko
+from sys import version
+import os
+import re
 import numpy as np
 import pandas as pd
-import re
+import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn import linear_model# import LogisticRegression
-from subprocess import check_output
+from sklearn import model_selection
+from sklearn import linear_model
+from sklearn import metrics
+import xgboost as xgb
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+def print_count(df):
+    stat = pd.DataFrame([df.dtypes, df.count(), df.isna().sum()], index=['dtypes', 'values', 'nans'])
+    print(stat.sort_values(by=['values'], axis=1, ascending=False))
 
-# fill NaNs with mean value
-def fillna_mean(feature: pd.Series):
-    mean = feature.mean()
-    feature.fillna(mean, inplace=True)
-
-# extract titles from passenger names
-def get_title(name):
-    title_search = re.search(' ([A-Za-z]+)\.', name)
-    if title_search:
-        return title_search.group(1)
-    return ''
-
-# print stats by feature
-def print_feature(df:pd.DataFrame, label:str):
-    ratio = df[label].value_counts() / df.shape[0]
-    print( ratio.apply(lambda x: '{:.2f}'.format(x)).to_csv(header=True, sep='\t') )
-
-# load data and setup features
-def prepare_data(name: str) -> pd.Series:
-    # load data
-    df = pd.read_csv('../input/' + name + '.csv')
-    print(name, df.shape)
-    # fill in the missing values
-    fillna_mean(df['Fare'])
-    fillna_mean(df['Age'])
-    fillna_mean(df['SibSp'])
-    fillna_mean(df['Parch'])
-    # new features - FamilySize
-    df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
-    print_feature(df, 'FamilySize')
-    # new features - Alone
-    df['Alone'] = 0
-    df.loc[df['FamilySize'] == 1, 'Alone'] = 1
-    print_feature(df, 'Alone')
-    # new feature - Title
-    df['Title'] = df['Name'].apply(get_title)
-    df['Title'].replace('Mlle', 'Miss', inplace=True)
-    df['Title'].replace('Ms', 'Miss', inplace=True)
-    df['Title'].replace('Mme', 'Mrs', inplace=True)
-    df['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare', inplace=True)
-    print_feature(df, 'Title')
+def print_value_counts(feature):
+    df = pd.DataFrame([feature.value_counts()], index=[feature.name])
+    df['nan'] = feature.isna().sum()
     return df
 
-train = prepare_data('train')
-test = prepare_data('test')
+def encode_cat(df:pd.DataFrame, label:str):
+    target = label.lower() + '_cat'
+    if target in df:
+        df.drop(columns=target, inplace=True)
+    notna = df[label].notna()
+    y = df[notna].loc[:, label]
+    df.loc[notna, target] = LabelEncoder().fit_transform(y).astype('int32')
+    print_value_counts(df[target])
 
-# Preprocess data
-def encode_labels(label: str):
-    le = LabelEncoder()
-    train[label].fillna('', inplace=True)
-    test[label].fillna('', inplace=True)
-    le.fit(train[label])
-    train.loc[:, label] = le.transform(train[label])
-    test.loc[:, label] = le.transform(test[label])
+def infer(df:pd.DataFrame, params, features:np.array, target:str):
+    # select training data and fit regressor
+    train = df[df[target].notna()]
+    x = train.loc[:, features]
+    y = train.loc[:, target]
+    regressor = xgb.XGBRegressor(n_jobs=4)
+    grid = model_selection.GridSearchCV(regressor, params, cv=5).fit(x, y)
+    print('score', grid.best_score_)
+    print('params', grid.best_params_)  
+    regressor = grid.best_estimator_    
+    
+    # predict missing target values
+    na_mask = df[target].isna()
+    predict = df[na_mask]
+    x_predict = predict.loc[:, features]
+    y_predict = regressor.predict(x_predict)
 
-encode_labels('Sex')
-encode_labels('Embarked')
-encode_labels('Title')
+    # create new feature
+    new_feature = target + '_'
+    df[new_feature] = df[target]
+    df.loc[na_mask, new_feature] = y_predict
+    df[new_feature].plot.kde()
 
-# prepare train for fitting
-# other features appears useless
-features = ['Age', 'Sex', 'Pclass', 'Fare', 'Parch', 'Alone']
-X = train.loc[:, features]
-Y = train.loc[:, 'Survived']
-XTest = test.loc[:, features]
-XTrain, XValid, YTrain, YValid = train_test_split(X, Y, test_size=0.2, random_state=40)
+    # return feature importance
+    #feature_importance = pd.DataFrame({'feature':features, 'importance':regressor.feature_importances_})
+    #return feature_importance.sort_values(by='importance', ascending=False)
+
+def infer_cat(df, params, features, target:str):
+    # select training data and classifier
+    train = df[df[target].notna()]
+    x = train.loc[:, features]
+    y = train.loc[:, target]
+    estimator = xgb.XGBClassifier(n_jobs=4)
+    grid = model_selection.GridSearchCV(estimator, params, cv=3).fit(x, y)
+    print('score', grid.best_score_)
+    print('params', grid.best_params_)  
+    estimator = grid.best_estimator_  
+    
+    # predict missing target values 
+    na = df[target].isna()
+    x_predict = df[na].loc[:, features]
+    y_predict = estimator.predict(x_predict)
+    
+    # create new feature
+    new_feature = target + '_'
+    df[new_feature] = df[target]
+    df.loc[na, new_feature] = y_predict
+    df[new_feature] = df[new_feature].astype('int64')
+    print_value_counts(df[new_feature])
 
 
-# Fit logistic regression using scikit
-LR = linear_model.LogisticRegression(C=1000, solver='lbfgs', max_iter=1000)
-LR.fit(X=XTrain, y=YTrain)
+def main():
+  print(os.listdir("../input"))
 
-def accuracy(Y: np.array, yPred: np.array) -> float:
-  return np.sum(yPred==Y) / len(Y)
+  # Load and merge datasets
+  train = pd.read_csv('../input/train.csv')
+  test = pd.read_csv('../input/test.csv')
+  data = train.append(test, sort=False)
+  print('\nMeet data:\n', data.sample(5))
 
-# Use model to predict on training and validation sets
-print('     Train accuracy', accuracy(YTrain, LR.predict(XTrain)))
-print('Validation accuracy', accuracy(YValid, LR.predict(XValid)))
+  # Look at types and incomplete features
+  print_count(data)
+  # Features to make categorical:
+  #    - Name
+  #    - Sex
+  #    - Ticket
+  #    - Embarked
+  # Incomplete features are:
+  #    - Fare
+  #    - Embarked
+  #    - Age
+  #    - Cabin
 
-# Predict for test set
-# Create a Kaggle submission
-sub = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': LR.predict(XTest)})
-sub.to_csv('scikitLRExample.csv', index=False)
+  # Check tickets
+  encode_cat(data, 'Ticket')
+
+  # Create FamilySize and Alone features
+  data['family_size'] = data['SibSp'] + data['Parch'] + 1
+  data['alone'] = 0
+  data.loc[data['family_size'] == 1, 'alone'] = 1
+  print_value_counts(data['alone'])
+
+  # Encode Sex
+  encode_cat(data, 'Sex')
+
+  # ## Extract Title from Name
+  data['title'] = data['Name'].str.extract(r', (.*?)\.', expand=False)
+
+  print_value_counts(data['title'])
+  # See [english honorifics](https://en.wikipedia.org/wiki/English_honorifics) for reference.
+  data['title'].replace(['Mlle', 'Ms'], 'Miss', inplace=True)
+  data['title'].replace(['Mme', 'Lady', 'Countess', 'Dona', 'the Countess'], 'Mrs', inplace=True)
+  data['title'].replace(['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer'], 'Mr', inplace=True)
+  print_value_counts(data['title'])
+
+  encode_cat(data, 'title')
+
+  ## Fix Fare
+  params = {'max_depth': [2, 3, 4],
+            'learning_rate': [0.3, 0.4, 0.5],
+            'n_estimators': [150, 170, 190]}
+  fare_features = ['Pclass', 'SibSp', 'sex_cat', 'title_cat', 'ticket_cat', 'family_size']
+  infer(data, params, fare_features, 'Fare')
+
+  # ## Encode and fix Embarked
+  encode_cat(data, 'Embarked')
+  params = {'max_depth': [3, 4, 5],
+            'learning_rate': [0.4, 0.5, 0.6],
+            'n_estimators': [400, 500, 600]}
+  emb_features = np.append(fare_features, 'Fare_')
+  infer_cat(data, params, emb_features, 'embarked_cat')
+
+  # ## Fix Age
+  infer(data, params, ['Pclass', 'SibSp', 'sex_cat', 'title_cat', 'ticket_cat', 'family_size', 'Fare_'], 'Age')
+
+  # ## Finally predict Survived
+  infer_cat(data, params, ['Pclass', 'SibSp', 'Parch', 'title_cat', 'sex_cat', 'Fare_', 'Age_', 'embarked_cat'], 'Survived')
+  na_mask = data['Survived'].isna()
+
+  # create a Kaggle submission
+  sub = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': data[na_mask].loc[:, 'Survived_']})
+  sub.to_csv('submission.csv', index=False)
+  print('Thanks!!!')
+
+if __name__=='__main__':
+  main()
