3e358f68804fea23302bf8c909ca45282b0b8a5c
Anton <apanchenko@gmail.com>
2019-05-23 00:00:40 +0300

    little refactor

--- titanic/src/titanic.py
+++ titanic/src/titanic.py
@@ -1,20 +1,20 @@
 # Anton Panchenko 2019-05
+import xgboost as xgb
+import sklearn as sl
+from sklearn import linear_model
+from sklearn import model_selection
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
 import sys
 print(' python', sys.version)
 
-import numpy as np
 print('  numpy', np.__version__)
 
-import pandas as pd
 print(' pandas', pd.__version__)
 
-from sklearn import preprocessing
-from sklearn import model_selection
-from sklearn import linear_model
-import sklearn as sl
 print('sklearn', sl.__version__)
 
-import xgboost as xgb
 print('xgboost', xgb.__version__)
 
 
@@ -25,10 +25,12 @@ class Smurf:
         self.data = train.append(test, sort=False)
         print('\nMeet data:\n', self.data.sample(5))
         # look at types and incomplete features
-        print('\nTypes and counts:\n', self.count(self.data))
+        print('\nTypes and counts:\n', self.count())
 
-    def count(self, df):
-        stat = pd.DataFrame([df.dtypes, df.count(), df.isna().sum()], index=['dtypes', 'values', 'nans'])
+    def count(self):
+        stat = pd.DataFrame(
+            [self.data.dtypes, self.data.count(), self.data.isna().sum()],
+            index=['dtypes', 'values', 'nans'])
         return stat.sort_values(by=['values'], axis=1, ascending=False)
 
     def print_value_counts(self, msg, feature):
@@ -37,46 +39,46 @@ class Smurf:
         df['nan'] = feature.isna().sum()
         print(df)
 
-    def encode_cat(self, label:str):
+    def encode_cat(self, label: str):
         target = label.lower() + '_cat'
         notna = self.data[label].notna()
         y = self.data[notna].loc[:, label]
         self.data.loc[notna, target] = sl.preprocessing.LabelEncoder().fit_transform(y).astype('int32')
         self.print_value_counts('\nEncode categorical \'%s\':' % label, self.data[target])
 
-    def infer(self, df, params, features:np.array, target:str):
+    def infer(self, params, features: np.array, target: str):
         if self.use_xgb:
-            self.infer_xgb(df, params, features, target)
+            self.infer_xgb(params, features, target)
         else:
-            self.infer_linear(df, features, target)
+            self.infer_linear(features, target)
 
-    def infer_linear(self, df, features:np.array, target:str):
+    def infer_linear(self, features: np.array, target: str):
         new_feature = target.lower() + '_'
         print('find %s(%s):' % (new_feature, features))
         # select training data and fit regressor
-        train = df[df[target].notna()]
+        train = self.data[self.data[target].notna()]
         x = train.loc[:, features]
         y = train.loc[:, target]
-        
+
         model = linear_model.LinearRegression()
         model.fit(X=x, y=y)
 
         # predict missing target values
-        na_mask = df[target].isna()
-        predict = df[na_mask]
+        na_mask = self.data[target].isna()
+        predict = self.data[na_mask]
         x_predict = predict.loc[:, features]
         y_predict = model.predict(x_predict)
 
         # create new feature
-        df[new_feature] = df[target]
-        df.loc[na_mask, new_feature] = y_predict
-        #df[new_feature].plot.kde()
+        self.data[new_feature] = self.data[target]
+        self.data.loc[na_mask, new_feature] = y_predict
+        # self.data[new_feature].plot.kde()
 
-    def infer_xgb(self, df, params, features:np.array, target:str):
+    def infer_xgb(self, params, features: np.array, target: str):
         new_feature = target.lower() + '_'
         print('\nInfer %s(%s):' % (new_feature, features))
         # select training data and fit regressor
-        train = df[df[target].notna()]
+        train = self.data[self.data[target].notna()]
         x = train.loc[:, features]
         y = train.loc[:, target]
         regressor = xgb.XGBRegressor(n_jobs=4)
@@ -84,34 +86,34 @@ class Smurf:
         print('score', grid.best_score_)
         print('best params', grid.best_params_)
         regressor = grid.best_estimator_
-        
+
         # predict missing target values
-        na_mask = df[target].isna()
-        predict = df[na_mask]
+        na_mask = self.data[target].isna()
+        predict = self.data[na_mask]
         x_predict = predict.loc[:, features]
         y_predict = regressor.predict(x_predict)
 
         # create new feature
-        df[new_feature] = df[target]
-        df.loc[na_mask, new_feature] = y_predict
-        #df[new_feature].plot.kde()
+        self.data[new_feature] = self.data[target]
+        self.data.loc[na_mask, new_feature] = y_predict
+        # self.data[new_feature].plot.kde()
 
         # show feature importance
-        feature_importance = pd.DataFrame({'feature':features, 'importance':regressor.feature_importances_})
+        feature_importance = pd.DataFrame({'feature': features, 'importance': regressor.feature_importances_})
         print('Feature importance:')
         print(feature_importance.sort_values(by='importance', ascending=False))
 
-    def infer_cat(self, df, params, features, target:str):
+    def infer_cat(self, params, features, target: str):
         if self.use_xgb:
-            self.infer_cat_xgb(df, params, features, target)
+            self.infer_cat_xgb(params, features, target)
         else:
-            self.infer_cat_linear(df, params, features, target)
+            self.infer_cat_linear(params, features, target)
 
-    def infer_cat_xgb(self, df, params, features, target:str):
+    def infer_cat_xgb(self, params, features, target: str):
         new_feature = target.lower() + '_'
-        print('\nInfer categorical %s(%s):' % (new_feature, features))  
+        print('\nInfer categorical %s(%s):' % (new_feature, features))
         # select training data and classifier
-        train = df[df[target].notna()]
+        train = self.data[self.data[target].notna()]
         x = train.loc[:, features]
         y = train.loc[:, target]
         estimator = xgb.XGBClassifier(n_jobs=4)
@@ -119,44 +121,45 @@ class Smurf:
         print('score', grid.best_score_)
         print('params', grid.best_params_)
         estimator = grid.best_estimator_
-        
-        # predict missing target values 
-        na = df[target].isna()
-        x_predict = df[na].loc[:, features]
+
+        # predict missing target values
+        na = self.data[target].isna()
+        x_predict = self.data[na].loc[:, features]
         y_predict = estimator.predict(x_predict)
-        
+
         # create new feature
-        df[new_feature] = df[target]
-        df.loc[na, new_feature] = y_predict
-        df[new_feature] = df[new_feature].astype('int64')
-        self.print_value_counts('', df[new_feature])
+        self.data[new_feature] = self.data[target]
+        self.data.loc[na, new_feature] = y_predict
+        self.data[new_feature] = self.data[new_feature].astype('int64')
+        self.print_value_counts('', self.data[new_feature])
 
         # show feature importance
-        feature_importance = pd.DataFrame({'feature':features, 'importance':estimator.feature_importances_})
+        feature_importance = pd.DataFrame(
+            {'feature': features, 'importance': estimator.feature_importances_})
         print('Feature importance:')
         print(feature_importance.sort_values(by='importance', ascending=False))
 
-    def infer_cat_linear(self, df, params, features, target:str):
+    def infer_cat_linear(self, params, features, target: str):
         new_feature = target.lower() + '_'
-        print('\nInfer categorical %s(%s):' % (new_feature, features))  
+        print('\nInfer categorical %s(%s):' % (new_feature, features))
         # select training data and classifier
-        train = df[df[target].notna()]
+        train = self.data[self.data[target].notna()]
         x = train.loc[:, features]
         y = train.loc[:, target]
-        
+
         model = linear_model.LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=1000)
         model.fit(X=x, y=y)
-        
-        # predict missing target values 
-        na = df[target].isna()
-        x_predict = df[na].loc[:, features]
+
+        # predict missing target values
+        na = self.data[target].isna()
+        x_predict = self.data[na].loc[:, features]
         y_predict = model.predict(x_predict)
-        
+
         # create new feature
-        df[new_feature] = df[target]
-        df.loc[na, new_feature] = y_predict
-        df[new_feature] = df[new_feature].astype('int64')
-        self.print_value_counts('', df[new_feature])
+        self.data[new_feature] = self.data[target]
+        self.data.loc[na, new_feature] = y_predict
+        self.data[new_feature] = self.data[new_feature].astype('int64')
+        self.print_value_counts('', self.data[new_feature])
 
 
 class Titanic(Smurf):
@@ -191,45 +194,46 @@ class Titanic(Smurf):
 
     # Pay Fare (best score 0.8489)
     def fare(self):
-        #self.features = ['Pclass', 'title_cat', 'sex_cat', 'family', 'ticket_cat']
-        self.features = ['Pclass', 'title_cat', 'sex_cat']
+        self.features = ['Pclass', 'title_cat', 'sex_cat', 'family', 'ticket_cat']
+        #self.features = ['Pclass', 'title_cat', 'sex_cat']
         params = {'max_depth': [2, 3, 4],
-                'learning_rate': [0.3, 0.4, 0.5],
-                'n_estimators': [150, 170, 190]}
-        self.infer(self.data, params, self.features, 'Fare')
+                  'learning_rate': [0.3, 0.4, 0.5],
+                  'n_estimators': [150, 170, 190]}
+        self.infer(params, self.features, 'Fare')
 
     # Encode and fix Embarked (best score 0.9479)
     def embarked(self):
         self.encode_cat('Embarked')
         params = {'max_depth': [3, 4, 5],
-                'learning_rate': [0.4, 0.5, 0.6],
-                'n_estimators': [400, 500, 600]}
+                  'learning_rate': [0.4, 0.5, 0.6],
+                  'n_estimators': [400, 500, 600]}
         self.features = np.append(self.features, 'fare_')
-        self.infer_cat(self.data, params, self.features, 'embarked_cat')
+        self.infer_cat(params, self.features, 'embarked_cat')
 
     # Fix Age (best score 0.4230)
     def age(self):
         self.features = np.append(self.features, 'embarked_cat_')
         params = {'max_depth': [2, 3],
-                'learning_rate': [0.04, 0.05, 0.08],
-                'n_estimators': [90, 100, 120]}
-        self.infer(self.data, params, self.features, 'Age')        
+                  'learning_rate': [0.04, 0.05, 0.08],
+                  'n_estimators': [90, 100, 120]}
+        self.infer(params, self.features, 'Age')
 
     # Final glance at data
     def survived(self):
         # Finally predict Survived (best score 0.8350)
         features = np.append(self.features, 'age_')
         params = {'max_depth': [3, 4, 5],
-                'learning_rate': [0.1, 0.3],
-                'n_estimators': [70, 100, 300]}
-        self.infer_cat(self.data, params, features, 'Survived')
+                  'learning_rate': [0.1, 0.3],
+                  'n_estimators': [70, 100, 300]}
+        self.infer_cat(params, features, 'Survived')
         # create submission
         na_mask = self.data['Survived'].isna()
-        sub = pd.DataFrame({'PassengerId': self.test['PassengerId'], 'Survived': self.data[na_mask].loc[:, 'survived_']})
+        sub = pd.DataFrame(
+            {'PassengerId': self.test['PassengerId'], 'Survived': self.data[na_mask].loc[:, 'survived_']})
         sub.to_csv('submission.csv', index=False)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     titanic = Titanic(False)
     titanic.title()
     titanic.sex()
