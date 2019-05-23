1e03d0329bff73f22e0e1775ac8ed5dd1eb19a53
Anton Panchenko <apanchenko@gmail.com>
2019-05-23 16:38:22 +0300

    refactor infer

--- titanic/src/titanic.py
+++ titanic/src/titanic.py
@@ -61,47 +61,41 @@ class Smurf:
 
     def infer(self, params, features: np.array, label: str):
         print('Infer ', label)
+        # select training data
+        train = self.data[self.data[label].notna()]
+        x = train.loc[:, features]
+        y = train.loc[:, label]
+        xt, xv, yt, yv = train_test_split(x, y, test_size=0.2, random_state=40)
+
+        model = None
         if self.use_xgb:
-            self.infer_xgb(params, features, label)
+            model = self._infer_xgb(params, xt, xv, yt, yv)
         else:
-            self.infer_linear(features, label)
-
-    def infer_linear(self, features: np.array, label: str):
-        # select training data and fit regressor
-        train = self.data[self.data[label].notna()]
-        x = train.loc[:, features]
-        y = train.loc[:, label]
-
-        model = linear_model.LinearRegression()
-        model.fit(x, y)
-
-        # predict missing target values
-        na_mask = self.data[label].isna()
-        predict = self.data[na_mask]
-        x_predict = predict.loc[:, features]
-        self.data.loc[na_mask, label] = model.predict(x_predict)
-
-    def infer_xgb(self, params, features: np.array, label: str):
-        # select training data and fit regressor
-        train = self.data[self.data[label].notna()]
-        x = train.loc[:, features]
-        y = train.loc[:, label]
-        regressor = xgb.XGBRegressor(n_jobs=4)
-        grid = sl.model_selection.GridSearchCV(regressor, params, cv=5, iid=True).fit(x, y)
-        print('score', grid.best_score_)
-        print('best params', grid.best_params_)
-        regressor = grid.best_estimator_
+            model = self._infer_linear(xt, xv, yt, yv)
 
         # predict missing target values
         na = self.data[label].isna()
         predict = self.data[na]
         x_predict = predict.loc[:, features]
-        self.data.loc[na, label] = regressor.predict(x_predict)
+        self.data.loc[na, label] = model.predict(x_predict)
 
+    def _infer_linear(self, xt, xv, yt, yv):
+        model = linear_model.LinearRegression()
+        model.fit(xt, yt)
+        print('Score', model.score(xv, yv))
+        return model
+
+    def _infer_xgb(self, params, xt, xv, yt, yv):
+        regressor = xgb.XGBRegressor(n_jobs=4)
+        grid = sl.model_selection.GridSearchCV(regressor, params, cv=5, iid=True).fit(xt, yt)
+        print('score', grid.best_score_)
+        print('best params', grid.best_params_)
+        model = grid.best_estimator_
         # show feature importance
-        feature_importance = pd.DataFrame({'feature': features, 'importance': regressor.feature_importances_})
-        print('Feature importance:')
-        print(feature_importance.sort_values(by='importance', ascending=False))
+        #feature_importance = pd.DataFrame({'feature': features, 'importance': model.feature_importances_})
+        #print('Feature importance:')
+        #print(feature_importance.sort_values(by='importance', ascending=False))
+        return model
 
     def infer_cat(self, params, features, label: str):
         print('Infer categorical', label)
@@ -113,12 +107,9 @@ class Smurf:
 
         model = None
         if self.use_xgb:
-            model = self.infer_cat_xgb(params, features, label)
+            model = self._infer_cat_xgb(params, xt, xv, yt, yv)
         else:
-            model = self.infer_cat_linear(xt, yt)
-
-        print('     Train accuracy', self.accuracy(yt, model.predict(xt)))
-        print('Validation accuracy', self.accuracy(yv, model.predict(xv)))
+            model = self._infer_cat_linear(xt, xv, yt, yv)
 
         # predict missing target values
         na = self.data[label].isna()
@@ -127,26 +118,23 @@ class Smurf:
         self.data[label] = self.data[label].astype('int32')
         print(self.value_counts(label))        
 
-    def infer_cat_xgb(self, params, features, label: str):
-        # select training data and classifier
-        train = self.data[self.data[label].notna()]
-        x = train.loc[:, features]
-        y = train.loc[:, label]
+    def _infer_cat_xgb(self, params, xt, xv, yt, yv):
         model = xgb.XGBClassifier(n_jobs=4)
-        grid = sl.model_selection.GridSearchCV(model, params, cv=3).fit(x, y)
+        grid = sl.model_selection.GridSearchCV(model, params, cv=3).fit(xt, yt)
         print('score', grid.best_score_)
         print('params', grid.best_params_)
         model = grid.best_estimator_
 
         # show feature importance
-        feature_importance = pd.DataFrame({'feature': features, 'importance': model.feature_importances_})
-        print('Feature importance:')
-        print(feature_importance.sort_values(by='importance', ascending=False))
+        #feature_importance = pd.DataFrame({'feature': features, 'importance': model.feature_importances_})
+        #print('Feature importance:')
+        #print(feature_importance.sort_values(by='importance', ascending=False))
         return model
 
-    def infer_cat_linear(self, xt, yt):
+    def _infer_cat_linear(self, xt, xv, yt, yv):
         model = linear_model.LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=10000)
         model.fit(xt, yt)
+        print('Accuracy', model.score(xv, yv))
         return model
 
 
@@ -160,13 +148,13 @@ class Titanic(Smurf):
     def title(self):
         self.print_cutline()
         # See english honorifics (https://en.wikipedia.org/wiki/English_honorifics) for reference.
-        self.data['title'] = self.data['Name'].str.extract(r', (.*?)\.', expand=False)
-        print('Extract title from name:')
-        print(self.value_counts('title'))
-        self.data['title'].replace(['Mlle', 'Ms'], 'Miss', inplace=True)
-        self.data['title'].replace(['Mme', 'Lady', 'Countess', 'Dona', 'the Countess'], 'Mrs', inplace=True)
-        self.data['title'].replace(['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer'], 'Mr', inplace=True)
-        self.encode_cat('title')
+        self.data['Title'] = self.data['Name'].str.extract(r', (.*?)\.', expand=False)
+        print('Extract Title from Name:')
+        print(self.value_counts('Title'), '\n')
+        self.data['Title'].replace(['Mlle', 'Ms'], 'Miss', inplace=True)
+        self.data['Title'].replace(['Mme', 'Lady', 'Countess', 'Dona', 'the Countess'], 'Mrs', inplace=True)
+        self.data['Title'].replace(['Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer'], 'Mr', inplace=True)
+        self.encode_cat('Title')
 
     # Encode Sex
     def sex(self):
@@ -176,9 +164,9 @@ class Titanic(Smurf):
     # Unite families
     def family(self):
         self.print_cutline()
-        self.data['family'] = self.data['SibSp'] + self.data['Parch'] + 1
-        print('SibSp + Parch = family:')
-        print(self.value_counts('family'))
+        self.data['Family'] = self.data['SibSp'] + self.data['Parch'] + 1
+        print('Family = SibSp + Parch + 1')
+        print(self.value_counts('Family'))
 
     # Check tickets
     def ticket(self):
@@ -188,12 +176,12 @@ class Titanic(Smurf):
     # Pay Fare (best score 0.8489)
     def fare(self):
         self.print_cutline()
-        self.features = ['Pclass', 'title', 'Sex', 'family', 'Ticket']
-        #self.features = ['Pclass', 'title_cat', 'sex_cat']
+        self.features = ['Pclass', 'Title', 'Sex', 'Family', 'Ticket']
         params = {'max_depth': [2, 3, 4],
                   'learning_rate': [0.3, 0.4, 0.5],
                   'n_estimators': [150, 170, 190]}
         self.infer(params, self.features, 'Fare')
+        self.features = np.append(self.features, 'Fare')
 
     # Encode and fix Embarked (best score 0.9479)
     def embarked(self):
@@ -202,27 +190,26 @@ class Titanic(Smurf):
         params = {'max_depth': [3, 4, 5],
                   'learning_rate': [0.4, 0.5, 0.6],
                   'n_estimators': [400, 500, 600]}
-        self.features = np.append(self.features, 'Fare')
         self.infer_cat(params, self.features, 'Embarked')
+        self.features = np.append(self.features, 'Embarked')
 
     # Fix Age (best score 0.4230)
     def age(self):
         self.print_cutline()
-        self.features = np.append(self.features, 'Embarked')
         params = {'max_depth': [2, 3],
                   'learning_rate': [0.04, 0.05, 0.08],
                   'n_estimators': [90, 100, 120]}
         self.infer(params, self.features, 'Age')
+        self.features = np.append(self.features, 'Age')
 
     # Final glance at data
     def survived(self):
         self.print_cutline()
         # Finally predict Survived (best score 0.8350)
-        features = np.append(self.features, 'Age')
         params = {'max_depth': [3, 4, 5],
                   'learning_rate': [0.1, 0.3],
                   'n_estimators': [70, 100, 300]}
-        self.infer_cat(params, features, 'Survived')
+        self.infer_cat(params, self.features, 'Survived')
         # create submission
         na = self.data['Survived'].isna()
         sub = pd.DataFrame({'PassengerId': self.test['PassengerId'], 'Survived': self.data[na].loc[:, 'Survived']})
